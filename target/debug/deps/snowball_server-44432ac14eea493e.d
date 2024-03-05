4de2e74fec7359db193219c0b08b79bfee96aca8
Penghao Shi <penghshi@tesla.com>
2024-03-05 09:25:37 +0800

    first commit

---
+++ target/debug/deps/snowball_server-44432ac14eea493e.d
@@ -0,0 +1,5 @@
+/Users/penghshi/github.com/LostInAurora/snowball/target/debug/deps/libsnowball_server-44432ac14eea493e.rmeta: snowball-server/src/main.rs
+
+/Users/penghshi/github.com/LostInAurora/snowball/target/debug/deps/snowball_server-44432ac14eea493e.d: snowball-server/src/main.rs
+
+snowball-server/src/main.rs:
---
+++ target/debug/deps/snowball_server-9c79e77303322bc7.d
@@ -0,0 +1,5 @@
+/Users/penghshi/github.com/LostInAurora/snowball/target/debug/deps/libsnowball_server-9c79e77303322bc7.rmeta: snowball-server/src/main.rs
+
+/Users/penghshi/github.com/LostInAurora/snowball/target/debug/deps/snowball_server-9c79e77303322bc7.d: snowball-server/src/main.rs
+
+snowball-server/src/main.rs:
